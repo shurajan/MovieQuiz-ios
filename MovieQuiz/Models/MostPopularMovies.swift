d16904b4fd30cd2718693731ce281c20d93d3784
shurajan <shurajangit@gmail.com>
2024-06-05 14:28:21 +0300

    sprint_6 initial commit - added network support

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,49 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Alexander Bralnin on 04.06.2024.
+//
+
+import Foundation
+
+struct MostPopularMovies: Codable {
+    let errorMessage: String
+    let items: [MostPopularMovie]
+}
+
+
+/*struct MostPopularMovie :Codable{
+    let title: String
+    let rating: String
+    let imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "fullTitle"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
+}*/
+
+struct MostPopularMovie: Codable {
+    let title: String
+    let rating: String
+    let imageURL: URL
+    
+    var resizedImageURL: URL {
+        let urlString = imageURL.absoluteString
+        let imageUrlString = urlString.components(separatedBy: "._")[0] + "._V0_UX600_.jpg"
+        
+        guard let newURL = URL(string: imageUrlString) else {
+            return imageURL
+        }
+        
+        return newURL
+    }
+    
+    private enum CodingKeys : String, CodingKey {
+        case title = "fullTitle"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
+}
--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -9,4 +9,7 @@ import Foundation
 
 protocol QuestionFactoryDelegate: AnyObject {               
     func didReceiveNextQuestion(question: QuizQuestion?)
+    func didFailToReceiveNextQuestion(with error: Error)
+    func didLoadDataFromServer()
+    func didFailToLoadData(with error: Error) 
 }
